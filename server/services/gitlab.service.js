808c87570d49bb75633f4a965fb10c0cadeffbec
fa_oumellal@esi.dz <islam.neddar.pro@gmail.com>
2021-01-17 15:36:11 +0100

    add others functions

--- server/services/gitlab.service.js
+++ server/services/gitlab.service.js
@@ -19,40 +19,54 @@ const handleSystemHook = async (hook)=>{
             await handleGroupCreated(hook)
             break
         case type_system_hooks.group.destroy :
-
+            await handleGroupRemoved(hook)
             break
         case type_system_hooks.group.rename :
+            await handleGroupRenamed(hook)
             break
         case type_system_hooks.project.create :
             //add project to sonarqube
+            await handleProjectCreated(hook)
             break
         case type_system_hooks.project.destroy :
             //add project to sonarqube
+            await handleProjectDestroyed(hook)
             break
         case type_system_hooks.project.rename :
             //add project to sonarqube
+            await handleProjectRenamed(hook)
             break
         case type_system_hooks.project.transfer :
             //add project to sonarqube
+            await handleProjectTrandfered(hook)
             break
         case type_system_hooks.project.update :
             //add project to sonarqube
+            await handleProjectUpdated(hook)
             break
         case type_system_hooks.user.create :
             //add userhandleSystemHook to sonarqube
+            await handleUserCreated(hook)
             break
         case type_system_hooks.user.destroy :
+            await handleUserDestroyed(hook)
             break
         case type_system_hooks.user.rename :
+            await handleUserRenamed(hook)
             break
         case type_system_hooks.user.add_to_group :
+            await handleUserAddedToGroup(hook)
             break
         case type_system_hooks.user.remove_from_group :
+            await handleUserRemovedFromGroup(hook)
             break
 
     }
 }
 
+// User => username = login
+// Group => name = name
+// Project => project/projectKey = path
 const handleGroupCreated = async (hook) => {
     console.log("group created")
     const {name} = hook
@@ -68,46 +82,60 @@ const handleGroupRemoved = async (hook) => {
 
 const handleGroupRenamed = async (hook) => {
     console.log("group renamed")
+
 }
 
 const handleProjectCreated = async (hook) => {
-    console.log("group created")
+    console.log("project created")
+    const {name,path,project_visibility} = hook
+    await sonarqubeService.addProject(name,path,project_visibility)
 }
 
 const handleProjectDestroyed = async (hook) => {
-    console.log("group created")
+    console.log("project destroyed")
+    const {path} = hook
+    await sonarqubeService.deleteProject(path)
 }
 
 const handleProjectRenamed = async (hook) => {
-    console.log("group created")
+    console.log("project renamed")
 }
 
 const handleProjectTrandfered = async (hook) => {
-    console.log("group created")
+    console.log("project transferd")
 }
 
 const handleProjectUpdated = async (hook) => {
-    console.log("group created")
+    console.log("group updated")
 }
 
 const handleUserCreated = async (hook) => {
-    console.log("group created")
+    console.log("user created")
+    const {email,username,name} = hook
+    await sonarqubeService.addUser(email,username,name,username)
 }
 
 const handleUserDestroyed = async (hook) => {
-    console.log("group created")
+    console.log("user_destroy")
+    const {username} = hook
+    await sonarqubeService.deleteUser(username)
 }
 
 const handleUserRenamed = async (hook) => {
-    console.log("group created")
+    console.log("user renamed")
+
 }
 
 const handleUserAddedToGroup = async (hook) => {
-    console.log("group created")
+    console.log("ad user to group")
+    const {group_name,user_username} = hook
+    await sonarqubeService.addUserToGroup(group_name,user_username)
 }
 
-const handle = async (hook) => {
-    console.log("group created")
+const handleUserRemovedFromGroup = async (hook) => {
+    console.log("remove user from group")
+    const {group_name,user_username} = hook
+    await sonarqubeService.removeUserFromGroup(group_name,user_username)
 }
 
 export const gitlabService = {
